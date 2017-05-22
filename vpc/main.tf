942f8fd87adaa1c052c7252330f2508eb110fa2d
Konstantin Chugalinskiy <k.chugalinskiy@fasten.com>
2017-05-22 17:30:07 +0300

    Removed aws_nat_gateway

--- vpc/main.tf
+++ vpc/main.tf
@@ -52,13 +52,6 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-resource "aws_nat_gateway" "main" {
-  count         = "${length(compact(keys(var.internal_subnets)))}"
-  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
-  subnet_id     = "${element(aws_subnet.external.*.id, count.index)}"
-  depends_on    = ["aws_internet_gateway.main"]
-}
-
 resource "aws_eip" "nat" {
   count = "${length(compact(keys(var.internal_subnets)))}"
   vpc   = true
